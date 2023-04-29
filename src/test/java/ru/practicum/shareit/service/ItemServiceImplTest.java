8690f75a369f9c3c154b90f412b11b7fd05d5391
Natalisv <nvselina@mail.ru>
2023-04-29 14:15:06 +0300

    ТЗ-15. Первая итерация

--- src/test/java/ru/practicum/shareit/service/ItemServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/service/ItemServiceImplTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import ru.practicum.shareit.booking.Status;
-import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.repository.BookingRepository;
 import ru.practicum.shareit.booking.service.BookingServiceImpl;
